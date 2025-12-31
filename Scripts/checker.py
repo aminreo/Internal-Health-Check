880243b99e9b6a9e3cb98d3cd50b04780e5cf47f
Amin <ayari.am@campus.lmu.de>
2025-12-31 17:51:45 +0100

    status code display

--- Scripts/checker.py
+++ Scripts/checker.py
@@ -1,11 +1,14 @@
 import json
+import requests
 
 
 def main():
     with open('Data/Services.JSON','r', encoding='utf-8') as json_obj:
         json_file=json.load(json_obj)
         for i in json_file:
-            print(i["url"])
+            # print(i["url"])
+            r= requests.head(i["url"])
+            print(r.status_code)
     return
 
 if __name__=="__main__":
